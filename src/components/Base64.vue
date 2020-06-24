7f07dab6d57231251584a07261810a9c3221480d
vladzur <vladzur@gmail.com>
2020-06-24 00:06:18 -0400

    Improve base64 tools

--- src/components/Base64.vue
+++ src/components/Base64.vue
@@ -1,33 +1,49 @@
 <template>
   <v-container>
-    <v-card>
-      <v-card-title>
-        Base 64 encoder
-      </v-card-title>
-      <v-card-text>
-        <v-textarea
-          v-model="textInput"
-          outlined
-        />
-        <p>
-          {{ base64Output }}
-        </p>
-      </v-card-text>
-    </v-card>
-    <v-card>
-      <v-card-title>
-        Base 64 decoder
-      </v-card-title>
-      <v-card-text>
-        <v-textarea
-          v-model="base64Input"
-          outlined
-        />
-        <p>
-          {{ textOutput }}
-        </p>
-      </v-card-text>
-    </v-card>
+    <v-tabs v-model="tabs">
+      <v-tab>Base 64 Encoder</v-tab>
+      <v-tab>Base 64 Decoder</v-tab>
+    </v-tabs>
+    <v-tabs-items v-model="tabs">
+      <v-tab-item>
+        <v-card>
+          <v-card-title>
+            Base 64 encoder
+          </v-card-title>
+          <v-card-text>
+            <v-textarea
+              v-model="textInput"
+              outlined
+            />
+            <v-textarea
+              v-model="base64Output"
+              outlined
+              filled
+              readonly
+            />
+          </v-card-text>
+        </v-card>
+      </v-tab-item>
+      <v-tab-item>
+        <v-card>
+          <v-card-title>
+            Base 64 decoder
+          </v-card-title>
+          <v-card-text>
+            <v-textarea
+              v-model="base64Input"
+              outlined
+            />
+            <v-textarea
+              v-model="textOutput"
+              outlined
+              filled
+              readonly
+            />
+          </v-card-text>
+        </v-card>
+      </v-tab-item>
+    </v-tabs-items>
   </v-container>
 </template>
 
@@ -36,7 +52,8 @@ export default {
   data: () => (
     {
       textInput: '',
-      base64Input: ''
+      base64Input: '',
+      tabs: null
     }
   ),
   computed: {
