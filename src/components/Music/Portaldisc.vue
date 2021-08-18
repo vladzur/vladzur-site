38c2f0b2ae1f92b40473a15df4953395af1b9781
vladzur <vladzur@gmail.com>
2021-08-18 16:27:43 -0400

    Code format

--- src/components/Music/Portaldisc.vue
+++ src/components/Music/Portaldisc.vue
@@ -2,14 +2,20 @@
   <div class="card">
   <div class="card-image">
     <figure class="image is-square">
-      <img src="/img/descarga.png" alt="Banner Descarga">
+      <img
+        src="/img/descarga.png"
+        alt="Banner Descarga"
+      >
     </figure>
   </div>
   <div class="card-content">
     <div class="media">
       <div class="media-left">
         <figure class="image is-48x48">
-          <img src="/img/logo_portaldisc.png" alt="Logo Portaldisc">
+          <img
+            src="/img/logo_portaldisc.png"
+            alt="Logo Portaldisc"
+          >
         </figure>
       </div>
       <div class="media-content">
@@ -20,7 +26,13 @@
 
     <div class="content">
       Puedes descargar, escuchar y compartir en
-      <a href="https://www.portaldisc.com/gratis.php?id=29273" target="_blank">Portaldisc</a>
+      <a
+        href="https://www.portaldisc.com/gratis.php?id=29273"
+        rel="noopener"
+        target="_blank"
+      >
+        Portaldisc
+      </a>
     </div>
   </div>
 </div>
--- src/components/Music/Spotify.vue
+++ src/components/Music/Spotify.vue
@@ -2,14 +2,20 @@
   <div class="card">
   <div class="card-image">
     <figure class="image is-square">
-      <img src="/img/Universum.jpg" alt="Arte de disco">
+      <img
+        src="/img/Universum.jpg"
+        alt="Arte de disco"
+      >
     </figure>
   </div>
   <div class="card-content">
     <div class="media">
       <div class="media-left">
         <figure class="image is-48x48">
-          <img src="/img/spotify_logo.png" alt="Logo Spotify">
+          <img
+            src="/img/spotify_logo.png"
+            alt="Logo Spotify"
+          >
         </figure>
       </div>
       <div class="media-content" >
@@ -20,7 +26,13 @@
 
     <div class="content">
       Puedes escuchar el disco y agregarlo a tu playlist más siniestra con
-      <a href="https://open.spotify.com/album/0BN0xpxAHezGXl0pkpVlz3?si=7cqPJSvUQyCUO8WJyPAGaw&dl_branch=1" target="_blank">Spotify</a>
+      <a
+        href="https://open.spotify.com/album/0BN0xpxAHezGXl0pkpVlz3?si=7cqPJSvUQyCUO8WJyPAGaw&dl_branch=1"
+        rel="noopener"
+        target="_blank"
+      >
+        Spotify
+      </a>
     </div>
   </div>
 </div>
